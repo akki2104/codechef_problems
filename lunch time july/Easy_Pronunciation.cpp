cea178a4a6209c595c73356ae52ae21829f9e58f
akki2104 <ay0832760@gmail.com>
2022-12-02 19:43:52 +0530

    easy pronunciation july lunch time

---
+++ lunch time july/Easy_Pronunciation.cpp
@@ -0,0 +1,69 @@
+#define rep0(i, n) for (int i = 0; i < n; i++)
+#define rep1(i, n) for (int i = 1; i < n; i++)
+#define pb(x) push_back(x)
+#define ppb(x) pop_back(x)
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define swappy(a, b) \
+    a ^= b;          \
+    b ^= a;          \
+    a ^= b;
+
+/* Things will definetly not be served to you in a plate, you have to take it forcefully what's yours (Wilson Fisk, Daredevil) */
+/* Forgetting is like a wound, the wound may heal but it has already left a scar. (Monkey D. Luffy, the future pirate king) */
+/* It's okay to loose your way, just don't loose the sight of what you have decided. (Ronoroa Zoro, the first crew mate of the future pirate king) */
+/* Life ins't a single battle, so it's okay to get beat up sometimes (Arthur Leywin's father, TBATE) */
+/* There comes a time when a man has to stand and fight! That time is when his friends’ dreams are being laughed at! And I won’t let you laugh at that! ( God Usopp) */
+/* Fools who don’t respect the past are likely to repeat it. (Akuma no ko Nico Robin) */
+/* There are times when a man has no choice but to act! ( Soul King Brook) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, flag = 0;
+        cin >> n;
+        string s;
+        cin >> s;
+        if (n <= 3)
+        {
+            flag = 0;
+        }
+        else
+        {
+            rep0(i, n - 3)
+            {
+                if (s[i] != 'a' && s[i] != 'e' && s[i] != 'i' && s[i] != 'o' && s[i] != 'u')
+                {
+                    if (s[i + 1] != 'a' && s[i + 1] != 'e' && s[i + 1] != 'i' && s[i + 1] != 'o' && s[i + 1] != 'u')
+                    {
+                        if (s[i + 2] != 'a' && s[i + 2] != 'e' && s[i + 2] != 'i' && s[i + 2] != 'o' && s[i + 2] != 'u')
+                        {
+                            if (s[i + 3] != 'a' && s[i + 3] != 'e' && s[i + 3] != 'i' && s[i + 3] != 'o' && s[i + 3] != 'u')
+                            {
+                                flag = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    flag = 0;
+                }
+            }
+        }
+        if (flag)
+            cout << "NO" << endl;
+        else
+            cout << "YES" << endl;
+    }
+}
+
+/* -coded by
+ The Fifth Emperor
+     Monkey D. Luffy */
